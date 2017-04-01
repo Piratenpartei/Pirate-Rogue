b25f619c750f82a1a279d266898870175893da99
Wolfgang Wiese <xwolf@xwolf.de>
2017-04-01 20:48:22 +0200

    Megasuperdupergeilefixivitäten #58 #44 Quotes verbessert

--- search.php
+++ search.php
@@ -11,44 +11,38 @@ get_header(); ?>
 
 
 <div class="content-wrap">
-
 	<div id="blog-wrap" class="blog-wrap cf">
-
 		<div id="primary" class="site-content cf" role="main">
-
 			<?php if ( have_posts() ) : ?>
 
 				<header class="archive-header">
-					<h1 class="archive-title"><?php echo absint($wp_query->found_posts); ?> <?php printf( esc_html__( 'Search Results for: %s', 'pirate-rogue'), '<span>' . get_search_query() . '</span>' ); ?></h1>
-					</header><!--end .archive-header -->
+                                    <h1 class="archive-title"><?php echo absint($wp_query->found_posts); ?> <?php printf( esc_html__( 'Search Results for: %s', 'pirate-rogue'), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</header><!--end .archive-header -->
 
-					<?php
-					// Start the loop.
-					while ( have_posts() ) : the_post();
+                                    <?php
+                                    while ( have_posts() ) : 
+                                        the_post();
+                                        get_template_part( 'content' );
+                                    endwhile;
 
-					get_template_part( 'content' );
+                                    // Previous/next page navigation.
+                                    the_posts_pagination( array(
+                                    'next_text' => '<span aria-hidden="true" class="meta-nav">' . esc_html__( 'Older', 'pirate-rogue') . '</span> ' .
+                                    '<span class="screen-reader-text">' . esc_html__( 'Older Posts', 'pirate-rogue') . '</span> ',
+                                    'prev_text' => '<span aria-hidden="true" class="meta-nav">' . esc_html__( 'Newer', 'pirate-rogue') . '</span> ' .
+                                    '<span class="screen-reader-text">' . esc_html__( 'Newer Posts', 'pirate-rogue') . '</span> ' ,
+                                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . esc_html__( 'Page', 'pirate-rogue') . ' </span>',
+                                    ) );
 
-					// End the loop.
-				endwhile;
+                                    // If no content, include the "No posts found" template.
+                            else :
+                                    get_template_part( 'template-parts/content', 'none' );
+                            endif;
+                            ?>
 
-				// Previous/next page navigation.
-				the_posts_pagination( array(
-					'next_text' => '<span aria-hidden="true" class="meta-nav">' . esc_html__( 'Older', 'pirate-rogue') . '</span> ' .
-					'<span class="screen-reader-text">' . esc_html__( 'Older Posts', 'pirate-rogue') . '</span> ',
-					'prev_text' => '<span aria-hidden="true" class="meta-nav">' . esc_html__( 'Newer', 'pirate-rogue') . '</span> ' .
-					'<span class="screen-reader-text">' . esc_html__( 'Newer Posts', 'pirate-rogue') . '</span> ' ,
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . esc_html__( 'Page', 'pirate-rogue') . ' </span>',
-					) );
+                            </div><!-- end #primary -->
 
-					// If no content, include the "No posts found" template.
-					else :
-						get_template_part( 'template-parts/content', 'none' );
-					endif;
-					?>
-
-				</div><!-- end #primary -->
-
-				<?php get_sidebar(); ?>
+                            <?php get_sidebar(); ?>
 
 			</div><!-- end .blog-wrap -->
 		</div><!-- end .content-wrap -->
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -15,21 +15,15 @@
 
 	<div class="entry-content">
 
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'pirate-rogue'), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'pirate-rogue'); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'pirate-rogue'); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
+            <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+                    <p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'pirate-rogue'), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+            <?php elseif ( is_search() ) : ?>
+                    <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'pirate-rogue'); ?></p>
+                    <?php get_search_form(); ?>
+            <?php else : ?>
+                    <p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'pirate-rogue'); ?></p>
+                    <?php get_search_form(); ?>
+            <?php endif; ?>
 
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
